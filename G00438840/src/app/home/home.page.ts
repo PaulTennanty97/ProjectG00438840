7a92a8f92dbf1c9b189c8deb86ca8356f43a26ac
PaulTennanty97 <p.tennanty1@gmail.com>
2026-01-01 16:02:17 +0000

    I updated HTML layout of the HomePage and began working on adding the Add to Favourites button to the header

--- G00438840/src/app/home/home.page.ts
+++ G00438840/src/app/home/home.page.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon } from '@ionic/angular/standalone';
+import {addIcons} from 'ionicons'; 
+import {heart} from 'ionicons/icons'; 
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonIcon],
 })
 export class HomePage {
-  constructor() {}
+  constructor() {
+    addIcons({ heart});
+  }
 }
+
