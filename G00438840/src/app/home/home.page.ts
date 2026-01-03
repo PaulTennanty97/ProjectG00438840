973befe34381b91b70e2e9fc627b1e73d6f1c8dd
PaulTennanty97 <p.tennanty1@gmail.com>
2026-01-03 08:30:37 +0000

    set up the connection to the spoonacular website. The homepage searchbar is working and displays the image and name of dishes.

--- G00438840/src/app/home/home.page.ts
+++ G00438840/src/app/home/home.page.ts
@@ -1,19 +1,47 @@
 import { Component } from '@angular/core';
-import { Router} from '@angular/router'
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButton } from '@ionic/angular/standalone';
-import {addIcons} from 'ionicons'; 
-import {heart, settings} from 'ionicons/icons'; 
+import { Router } from '@angular/router';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonIcon, IonButton, IonSearchbar, IonList, IonThumbnail, IonItem, IonLabel, IonCard } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { heart, settings } from 'ionicons/icons';
 import { HeaderComponent } from '../components/header/header.component';
+import { Http } from '../services/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  standalone: true, 
-  imports: [HeaderComponent, IonContent],
+  standalone: true,
+  imports: [HeaderComponent, IonContent, IonSearchbar, IonList, IonThumbnail, IonItem, IonLabel, IonButton, IonCard],
 })
 export class HomePage {
-  constructor(){}
-}
 
+  recipes: any[] = [];
+  private apiKey = "70759a4f7911402abcc53d3c51d3b759";
+
+  constructor(private mhs: Http) { }
+
+  ngOnInit() { // getting the JSON data from the url. 
+    this.getRecipes('');
+  }
+
+  handleInput(event: any) {
+    const query = event?.target.value.toLowerCase();
+    if (query && query.trim().length >= 3) {
+      this.getRecipes(query);
+    }
+  }
+  getRecipes(query: string) {
+    const url = `https://api.spoonacular.com/recipes/complexSearch?apiKey=${this.apiKey}&query=${query}&number=25`;
+    this.mhs.get(url).subscribe({
+      next: (data: any) => {
+        this.recipes = data.results;
+        console.log('Data received:', this.recipes);
+      },
+      error: (e: any) => {
+        console.error(e);
+      },
+      complete: () => { }
+    });
+  }
+}
 
