2761610c3cdb2c834bf2944de00de3d85ec31c5e
PaulTennanty97 <p.tennanty1@gmail.com>
2026-01-04 10:30:36 +0000

    Final review complete and GitLink.txt document included in src folder

--- G00438840/src/app/services/favouritesService.ts
+++ G00438840/src/app/services/favouritesService.ts
@@ -5,29 +5,34 @@ import { Preferences } from '@capacitor/preferences';
   providedIn: 'root',
 })
 export class FavouritesService {
+  // an array which holds favourite recipes. 
   private favourites: any[] = [];
 
   constructor(){
+    // when the page is loaded, any favoourites will be loaded by loadFavourites method. 
     this.loadFavourites();
   }
-
+// a method to add a recipe as a favourite. 
   async addFavourite(recipe: any){
     const favRecipe = this.favourites.find(fav => fav.id ===recipe.id); 
     if(!favRecipe){
       this.favourites.push(recipe); 
       console.log("Recipe addedd to Favourites", this.favourites); 
-      await this.saveToStorage(); 
+      await this.saveToStorage(); // updates the storage so the browser / phone can remember. 
 
     }
   }
+  // this method will return the current list of favourites at the time it is called. 
   getFavourites(){
     return this.favourites; 
   }
-  async removeFavourite(recipeID: any){
+  // this method will remove a recipe as a favourite using the recipe ID
+  async removeFavourite(recipeID: number){
     this.favourites = this.favourites.filter(fav => fav.id != recipeID);
-    await this.saveToStorage(); 
+    await this.saveToStorage(); // the changes are asaved to the storage so the browser / phone can remember. 
     console.log('Recipe removed'); 
   }
+  // this method will retrieve the favorites list from Capacitor preferences and convert the JSON into an object /array.
   async loadFavourites() {
     console.log("Loading favourites from storage.")
     const {value} = await Preferences.get({ key: 'my_fav_recipes'}); 
@@ -39,6 +44,7 @@ export class FavouritesService {
       console.log('No favourites found');
     }
   }
+// this method will save the favourites list to storage so the browser / phone. 
   async saveToStorage(){
     console.log('Saving to Ionic Storage');
     await Preferences.set({
@@ -47,6 +53,7 @@ export class FavouritesService {
     });
     console.log('Save complete!')
   }
+// this method performs a boolean check as to whether the recipe is already a favourite recipe or not. 
   isFavourite(recipeId: number): boolean {
     return this.favourites.some(fav => fav.id == recipeId);
   }
--- G00438840/src/app/services/settingsService.ts
+++ G00438840/src/app/services/settingsService.ts
@@ -6,28 +6,31 @@ import { BehaviorSubject } from 'rxjs';
   providedIn: 'root',
 })
 export class SettingsService {
+
+/* In this class, I rely on the use of BehaviourSubject to store and 
+communicate the unit of measurement preference.
+  https://eliteionic.com/tutorials/using-behaviorsubject-to-handle-asynchronous-loading-in-ionic/
+*/
   private UNIT_KEY = 'user_unit_pref'; 
   private unitSubject = new BehaviorSubject <'metric'|'us'>('metric'); 
   unit$ = this.unitSubject.asObservable(); 
 
   constructor(){
-    this.loadSavedUnit();
-    //this.getUnitPref();
+    this.loadSavedUnit(); // pulls the most recent unit of measurement preference from the local storage. 
   }
-
+// this method loads the user preference when the app starts up.
   private async loadSavedUnit(){
     const {value} = await Preferences.get({key: this.UNIT_KEY});
     const unit = (value as 'metric' |'us') || 'metric';
     console.log('SettingsService has loaded the unit of measurement from storage.')
     this.unitSubject.next(unit);
   }
-
+// this method retrieves the saved unit of measurement preference from local storage.
   async getUnitPref(){
     const {value} = await Preferences.get({key: this.UNIT_KEY});
     return (value as 'metric' |'us') || 'metric';
-    //const unit = (value as 'metric' |'us') || 'metric'; 
-    //this.unitSubject.next(unit); 
   }
+  // this method updated the unit of measurement preference in the local storage. 
   async setUnitPref(unit: 'metric' | 'us'){
     await Preferences.set({
       key: this.UNIT_KEY,
